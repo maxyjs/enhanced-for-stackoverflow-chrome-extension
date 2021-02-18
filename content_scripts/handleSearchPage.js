8706e20498548fc2ffb6afd61e4eb373ceefa01d
SokolovDADeveloper <sokolovdadeveloper@gmail.com>
2021-02-18 15:34:07 +0300

    add parse search words

--- content_scripts/handleSearchPage.js
+++ content_scripts/handleSearchPage.js
@@ -1,3 +1,28 @@
-(async function () {
-  await console.log("run 2")
+;(function () {
+  console.clear()
+  handleSearchPageSO().catch((error) => {
+    console.error("handleSearchPageSO error")
+    console.error(error)
+  })
 })();
+
+async function handleSearchPageSO() {
+  
+  const queryString = (new URL(document.location)).searchParams.get('q')
+  const resultsContainer = document.querySelector('.js-search-results > div')
+  const necessaryWords = getSearchWords(queryString)
+
+  function getSearchWords(queryString) {
+    const allWords = queryString.trim().split(/\s+/g)
+    const reg = /([\[])|(\.\.)|(^-)|(:)/
+    let words = []
+    
+    words = allWords.filter(word => {
+      const match = reg.test(word)
+      return !match
+    })
+
+    return words
+  }
+}
+
